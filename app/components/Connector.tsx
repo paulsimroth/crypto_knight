8bdbdcfd4c5afc2e26fea1214b327e21ee6771e4
Paul Simroth <mail@simroth.at>
2024-07-23 21:20:13 +0200

    edit to Connector Component to allow easy disconnect

--- app/components/Connector.tsx
+++ app/components/Connector.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { formatEthAddress } from "@/lib/wagmi";
 import { Button } from "./ui/button";
-import { useAccount, useConnect } from "wagmi";
+import { useAccount, useConnect, useDisconnect, useEnsName } from "wagmi";
 import { injected, metaMask } from "wagmi/connectors";
 import {
     DropdownMenu,
@@ -12,49 +12,66 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import Image from "next/image";
-import { Wallet } from "lucide-react";
+import { Ban, Wallet } from "lucide-react";
 
 function Connector() {
 
-    const { connect } = useConnect();
+    const { connectors, connect } = useConnect();
     const { address } = useAccount();
+    const { disconnect } = useDisconnect();
+    const { data: ensName } = useEnsName({ address });
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger>
                 <Button size="lg" variant={address ? "outline" : "default"}>
                     {address ? (
-                        formatEthAddress(address)
+                        ensName ? ensName : formatEthAddress(address)
                     ) : (
                         "Connect Wallet"
                     )}
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel>Connect your Wallet</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <Button onClick={() => connect({
-                        connector: metaMask()
-                    })} variant={"ghost"} className="flex items-center justify-start w-full gap-3">
-                        <Image
-                            src="/metamask_logo.svg"
-                            alt="MetaMask"
-                            width={35}
-                            height={35}
-                        />
-                        <span>MetaMask</span>
-                    </Button>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <Button onClick={() => connect({
-                        connector: injected()
-                    })} variant={"ghost"} className="flex items-center justify-start w-full gap-3">
-                        <Wallet />
-                        <span>Other</span>
-                    </Button>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
+
+            {address ? (
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>Options</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <Button onClick={() => disconnect()} variant={"ghost"} className="flex items-center justify-start w-full gap-3">
+                            <Ban className="text-destructive" />
+                            <span>Disconnect</span>
+                        </Button>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            ) : (
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>Connect your Wallet</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <Button onClick={() => connect({
+                            connector: metaMask()
+                        })} variant={"ghost"} className="flex items-center justify-start w-full gap-3">
+                            <Image
+                                src="/metamask_logo.svg"
+                                alt="MetaMask"
+                                width={35}
+                                height={35}
+                            />
+                            <span>MetaMask</span>
+                        </Button>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Button onClick={() => connect({
+                            connector: injected()
+                        })} variant={"ghost"} className="flex items-center justify-start w-full gap-3">
+                            <Wallet />
+                            <span>Other</span>
+                        </Button>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            )}
+
         </DropdownMenu>
     )
 };
